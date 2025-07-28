4e513da20070c512e69a019dd702365e3e583a49
Sowu Kelly <sowukelly67@gmail.com>
2025-07-28 15:21:27 +0200

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
 
+const token = localStorage.getItem('auth_token');
 const api = axios.create({
   baseURL: 'https://cb46111a28ca.ngrok-free.app/api/',
-  headers: {
-    'Authorization': `Token ${localStorage.getItem('token')}`,
+  headers: token ? {
+    'Authorization': `Token ${token}`,
     'Content-Type': 'application/json',
-  },
+  } : {
+    'Content-Type': 'application/json',
+  }
 })
 
 export default api
