db09727a3ec911eb5b43e8f3acdc227743d84b64
Sowu Kelly <sowukelly67@gmail.com>
2025-07-29 11:30:00 +0200

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -5,6 +5,7 @@ const api = axios.create({
   headers: {
     'Authorization': `Token ${localStorage.getItem('auth_token')}`,
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true'
   },
 })
 
