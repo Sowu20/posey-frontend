2f5a78fa0bf238ec41de37a2cb5c6f21aa6abb44
Sowu Kelly <sowukelly67@gmail.com>
2025-08-14 17:15:36 +0200

    posey_frontend

--- src/views/composants/AppHeader.vue
+++ src/views/composants/AppHeader.vue
@@ -31,8 +31,8 @@
             <!-- <span v-if="notifications.length" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
               {{ notifications.length }}
             </span> -->
-            <span v-if="notifications.length" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-              {{ notifications.length }}
+            <span v-if="unreadCount" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              {{ unreadCount }}
             </span>
           </div>
 
