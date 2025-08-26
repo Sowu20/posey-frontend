c986a4fb24ff3924aa797a358a6090fc8d3f5a08
Sowu Kelly <sowukelly67@gmail.com>
2025-08-26 18:53:59 +0200

    posey_frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const token = localStorage.getItem('auth_token');
 
 const api = axios.create({
-  baseURL: 'https://culture-cedar-briefing-impossible.trycloudflare.com/api/',
+  baseURL: 'https://landscape-waterproof-yo-ian.trycloudflare.com/api/',
   headers: {
     ...(token && { 'Authorization': `Token ${token}` }), // ou Bearer
     'Content-Type': 'application/json',
