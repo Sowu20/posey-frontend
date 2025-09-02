c54870eac62360b3eabe67bba8435af1a543136a
Sowu20 <sowukelly67@gmail.com>
2025-09-02 15:01:52 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const token = localStorage.getItem('auth_token');
 
 const api = axios.create({
-  baseURL: "https://c92c3a5e2b5b.ngrok-free.app/api/",
+  baseURL: "https://148c1876d80b.ngrok-free.app/api/",
   headers: {
     ...(token && { Authorization: `Token ${token}` }),
     "Content-Type": "application/json",
