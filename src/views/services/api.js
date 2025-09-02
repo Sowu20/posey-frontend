3cd722e5587bf6e3f6553c59d5794acbdcc6ce2b
Sowu20 <sowukelly67@gmail.com>
2025-09-02 09:18:27 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 const token = localStorage.getItem('auth_token');
 
 const api = axios.create({
-  baseURL: 'https://38312307dea2.ngrok-free.app/api/',
+  baseURL: "https://c4333a935387.ngrok-free.app/api/",
   headers: {
-    ...(token && { 'Authorization': `Token ${token}` }),
-    'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': 'true',
+    ...(token && { Authorization: `Token ${token}` }),
+    "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
