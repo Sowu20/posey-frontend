45a38c5e9358fc8aabc03d6dad6af7eb716e4094
Sowu20 <sowukelly67@gmail.com>
2025-09-09 09:37:09 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('auth_token');
 // const user = user?.access || null;
 
 const api = axios.create({
-  baseURL: "https://curriculum-ion-sa-gdp.trycloudflare.com/api/",
+  baseURL: "https://vertex-reaction-cms-stroke.trycloudflare.com/api/",
   headers: {
     ...(token && { Authorization: `Token ${token}` }),
     "Content-Type": "application/json",
