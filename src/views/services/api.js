75f651887b9181480f674a04e34cf54b72289213
Sowu20 <sowukelly67@gmail.com>
2025-09-12 10:38:20 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('auth_token');
 // const user = user?.access || null;
 
 const api = axios.create({
-  baseURL: "https://jane-release-overcome-annotation.trycloudflare.com/api/",
+  baseURL: "https://posing-emperor-ip-videos.trycloudflare.com/api/",
   headers: {
     ...(token && { Authorization: `Token ${token}` }),
     "Content-Type": "application/json",
