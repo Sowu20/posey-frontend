7ad9a27e42ae2f890de6c9e2aeb5f45f3bed91c0
Sowu20 <sowukelly67@gmail.com>
2025-09-15 08:40:15 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('auth_token');
 // const user = user?.access || null;
 
 const api = axios.create({
-  baseURL: "https://germany-shark-population-genetics.trycloudflare.com/api/",
+  baseURL: "https://ap-farm-america-infection.trycloudflare.com/api/",
   headers: {
     ...(token && { Authorization: `Token ${token}` }),
     "Content-Type": "application/json",
