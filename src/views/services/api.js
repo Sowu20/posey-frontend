c23a97c17d4c14cba728396473ab6d3a603655a3
Sowu20 <sowukelly67@gmail.com>
2025-09-24 14:53:28 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('auth_token');
 // const user = user?.access || null;
 
 const api = axios.create({
-  baseURL: "https://learned-sentences-tough-mat.trycloudflare.com/api/",
+  baseURL: "https://truck-nut-rolling-analyst.trycloudflare.com/api/",
   headers: {
     ...(token && { Authorization: `Token ${token}` }),
     "Content-Type": "application/json",
