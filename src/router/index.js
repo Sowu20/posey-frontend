ae8d24e646e788ee1aa03c17bfc30aaf80f8ce8a
Sowu20 <sowukelly67@gmail.com>
2025-09-26 09:54:13 +0000

    posey-frontend

--- src/router/index.js
+++ src/router/index.js
@@ -129,7 +129,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 })
 
