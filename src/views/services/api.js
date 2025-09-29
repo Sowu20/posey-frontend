a08c4f8b7aebf47fc2e892a0f49c2fbc8997e225
Sowu20 <sowukelly67@gmail.com>
2025-09-29 09:26:24 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('auth_token');
 // const user = user?.access || null;
 
 const api = axios.create({
-  baseURL: "https://consciousness-borders-commit-load.trycloudflare.com/api/",
+  baseURL: "https://routines-catalogs-etc-settle.trycloudflare.com/api/",
   headers: {
     ...(token && { Authorization: `Token ${token}` }),
     "Content-Type": "application/json",
