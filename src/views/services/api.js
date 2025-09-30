839e29e1566674383e3fdc7d31212456c0d3ad69
Sowu20 <sowukelly67@gmail.com>
2025-09-30 10:26:50 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('auth_token');
 // const user = user?.access || null;
 
 const api = axios.create({
-  baseURL: "https://racks-original-lloyd-hood.trycloudflare.com/api/",
+  baseURL: "https://craft-nascar-dial-live.trycloudflare.com/api/",
   headers: {
     ...(token && { Authorization: `Token ${token}` }),
     "Content-Type": "application/json",
