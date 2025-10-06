90c587321bdabab219de4029306ad2f91357de39
Sowu20 <sowukelly67@gmail.com>
2025-10-06 09:00:37 +0000

    posey-frontend

--- src/views/services/api.js
+++ src/views/services/api.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('auth_token');
 // const user = user?.access || null;
 
 const api = axios.create({
-  baseURL: "https://promotes-hearing-armor-nobody.trycloudflare.com/api/",
+  baseURL: "https://upon-allows-unix-projected.trycloudflare.com/api/",
   headers: {
     ...(token && { Authorization: `Token ${token}` }),
     "Content-Type": "application/json",
